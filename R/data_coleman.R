96e035e343175c89514ccdc42cd3dd4b208bc6e0
Tom <txm676@gmail.com>
2018-03-02 21:23:31 +0000

    coleman edit

--- R/data_coleman.R
+++ R/data_coleman.R
@@ -1,6 +1,6 @@
 #' A simulated species-site abundance matrix with site areas
 #'
-#' A l dataset in the correct mmSAR2 format:
+#' A dataset in the correct mmSAR2 format:
 #'
 #' @usage data(cole_sim)
 #' @format A list with two elements. The first element contains a species-site
