6098315d7c6e1750bc6b26af07303c64475daefd
Tom <txm676@gmail.com>
2018-03-02 21:22:56 +0000

    coleman edits

--- R/coleman.R
+++ R/coleman.R
@@ -1,3 +1,15 @@
+#First, the two functions which are derived from Coleman et al. (1982), which fit the model
+#and determine the standard deviation around the model’s predicted values:
+
+sa <- function(x, a){
+  sa <- (1 - x) ^ a
+  return(sa)
+}
+
+sa2 <- function(x, a){
+  sa2 <- (1 - x)^(2 * a)
+  return(sa2)
+}
 
 
 
