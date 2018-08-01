6bd9624b81cf19ea74fb6462cc783a149d39dfd8
Tom <txm676@gmail.com>
2018-08-01 11:37:42 +0100

    sar_multi workings

--- R/sar_conf_int.R
+++ R/sar_conf_int.R
@@ -8,7 +8,7 @@
 #' @examples
 #' #multi_sar fit
 #' data(galap)
-#' fit <- multi_sar(data = galap)
+#' fit <- sar_multi(data = galap)
 #' conf_fit <- sar_conf_int(fit)
 #' #simple sar fit
 #' data(galap)
@@ -18,10 +18,22 @@
 #' @export
 
 
-sar_conf_int <- function(){
+#n = number of iterations
+
+sar_conf_int <- function(fit, n, normaTest = "lillie",
+                         homoTest = "cor.fitted",
+                         neg_check = TRUE,
+                         alpha_normtest = 0.05,
+                         alpha_homotest = 0.05){
   
+  if (!"multi" %in% class(fit)) stop ("class of 'fit' should be 'multi'")
+  if (length(fit$details$mod_names) < 2) stop ("less than two models in the sar multi object")
   
+  wei <- fit$details$weights_ics
+  nams <- as.vector(names(wei))
   
+  #pick model name based on model weight
+  sm <- sample(nams, n, prob = wei)
   
   
   
