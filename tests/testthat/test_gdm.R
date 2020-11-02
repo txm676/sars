25d8d11d4937520687b7cc4f14d29bc9236b9702
Tom Matthews <txm676@gmail.com>
2020-11-02 11:01:29 +0000

    correcting bugs in gdm test

--- tests/testthat/test_gdm.R
+++ tests/testthat/test_gdm.R
@@ -7,7 +7,7 @@ test_that("gdm functions return correct results", {
   data(aegean)
   
   ###matches with sar_power
-  g2 <- gdm(galap, model = "power", mod_sel = TRUE)
+  g2 <- sars::gdm(galap, model = "power", mod_sel = TRUE)
   pow <- g2[[3]]
   pow_sars <- sar_power(galap)
   
@@ -16,14 +16,14 @@ test_that("gdm functions return correct results", {
   expect_equal(as.vector(round(pow_sars$par[1], 1)), 
                as.vector(round(exp(pow$m$getPars()[1]), 1)))
   expect_equal(pow_sars$AIC, AIC(pow))
-  expect_error(gdm(aegean), "Not enough columns/variables to fit GDM")
-  expect_error(gdm(galap, start_vals = c(5,4,3,6)), 
+  expect_error(sars::gdm(aegean), "Not enough columns/variables to fit GDM")
+  expect_error(sars::gdm(galap, start_vals = c(5,4,3,6)), 
                "start_vals should be a dataframe with 1 row and four columns")
-  expect_error(gdm(galap, model = "betap"), "provided model name not available")
-  expect_error(gdm(galap, mod_sel = "d"), 
+  expect_error(sars::gdm(galap, model = "betap"), "provided model name not available")
+  expect_error(sars::gdm(galap, mod_sel = "d"), 
                "mod_sel argument should be TRUE or FALSE")
   #matches with BAT values
-  BT <- BAT::gdm(galap$s, area = galap$a, time = galap$t)
+  BTP <- BAT::gdm(galap$s, area = galap$a, time = galap$t)
   expect_equal(as.vector(round(BTP[3,3], 2)), 
                as.vector(round(g2[[1]]$m$getAllPars()[3], 2)))
   expect_equal(as.vector(round(BTP[3,4], 2)), 
@@ -32,7 +32,7 @@ test_that("gdm functions return correct results", {
                as.vector(round(g2[[1]]$m$getAllPars()[2], 2)))
   
   ###matches with sar_loga
-  g3 <- gdm(galap, model = "loga", mod_sel = TRUE)
+  g3 <- sars::gdm(galap, model = "loga", mod_sel = TRUE)
   loga <- g3[[3]]
   loga_sars <- sar_loga(galap)
   
@@ -50,7 +50,7 @@ test_that("gdm functions return correct results", {
                as.vector(round(g3[[1]]$m$getAllPars()[2], 2)))
   
   ###matches with sar_linear
-  g4 <- gdm(galap, model = "linear", mod_sel = TRUE)
+  g4 <- sars::gdm(galap, model = "linear", mod_sel = TRUE)
   linear <- g4[[3]]
   lin_sars <- sar_linear(galap)
   
@@ -68,21 +68,21 @@ test_that("gdm functions return correct results", {
                as.vector(round(g4[[1]]$m$getAllPars()[2], 2)))
   
   ###all model comparison matches with individual model fits
-  g5 <- gdm(galap, model = "all", mod_sel = FALSE)
+  g5 <- sars::gdm(galap, model = "all", mod_sel = FALSE)
   expect_equal(AIC(g4[[1]]), AIC(g5[[2]]))
   expect_equal(AIC(g2[[1]]), AIC(g5[[3]]))
   expect_equal(AICcmodavg::AICc(g3[[1]]), AICcmodavg::AICc(g5[[1]]))
   #matches with BAT values (Delta AIC of linear vs. power)
-  expect_equal(round(AIC(g5[[2]]) - AIC(g5[[3]]), 2), round(BT[1,7], 2)) 
+  expect_equal(round(AIC(g5[[2]]) - AIC(g5[[3]]), 2), round(BTP[1,7], 2)) 
   expect_equal(round(AICcmodavg::AICc(g5[[2]]) - AICcmodavg::AICc(g5[[3]]), 2), 
-               round(BT[1,9], 2)) 
+               round(BTP[1,9], 2)) 
   
-  g5b <- gdm(galap, model = "all", mod_sel = TRUE)
+  g5b <- sars::gdm(galap, model = "all", mod_sel = TRUE)
   expect_equal(length(g5b), 3)
   expect_equal(length(g5b[[1]]), 4)
   
   ###linear power version
-  g6 <- gdm(galap, model = "lin_pow", mod_sel = TRUE)
+  g6 <- sars::gdm(galap, model = "lin_pow", mod_sel = TRUE)
   linP <- g6[[3]]
   lin_sars <- lin_pow(galap)
   
@@ -91,6 +91,6 @@ test_that("gdm functions return correct results", {
   expect_equal(as.vector(round(linP$coefficients[1], 2)), 
                as.vector(round(lin_sars$Model$coefficients[1], 2)))
   
-  g6b <- gdm(galap, model = "lin_pow", mod_sel = FALSE)
+  g6b <- sars::gdm(galap, model = "lin_pow", mod_sel = FALSE)
   expect_equal(class(g6b), c("gdm", "lm"))
 })
