03f7ff64fec0160b50b5a65d2b4e1a9a8948b5aa
Tom Matthews <txm676@gmail.com>
2025-01-18 19:09:44 +0000

    p13

---
+++ tests/testthat/test_coleman.R
@@ -0,0 +1,11 @@
+context("coleman")
+
+test_that("coleman main returns correct results", {
+  data(cole_sim)
+  fit <- coleman(cole_sim[[1]], cole_sim[[2]])
+  expect_equal(round(sum(fit$Predicted_values), 2), 99.60)
+  expect_output(str(fit), "List of 4")
+  expect_is(fit, "coleman")
+  expect_no_error(plot(fit))
+  expect_no_error(plot(fit, ModTitle = "A"))
+})
