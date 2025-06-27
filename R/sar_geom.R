04d132da7fe088895e7d838f56896abbb0ac40f5
txm676 <txm676@gmail.com>
2025-06-27 19:04:34 +0100

    sar_geom workings

--- R/sar_geom.R
+++ R/sar_geom.R
@@ -5,43 +5,43 @@
 #' 
 #' #' @importFrom sf st_geometry st_buffer st_intersection st_area st_intersects
 #' 
-#' filter_points_in_expanding_circles <- function(points_sf, 
-#'                                                radius_vector, 
+#' filter_points_in_expanding_circles <- function(points_sf,
+#'                                                radius_vector,
 #'                                                convex_hull) {
 #'   # Randomly select one point from points_sf
 #'   selected_point <- points_sf[sample(1:nrow(points_sf), 1), ]
-#'   
+#' 
 #'   # Initialize a list to store points within each circle
 #'   points_within_circles <- list()
-#'   
+#' 
 #'   # Loop through the radius_vector
 #'   for (radius in radius_vector) {
 #'     # Create a buffer (circle) around the selected point with the current radius
 #'     circle <- st_geometry(st_buffer(selected_point, dist = radius))
-#'     
+#' 
 #'     # Calculate the intersection of the circle with the convex_hull
-#'     intersection <- st_intersection(circle, convex_hull) 
-#'     
+#'     intersection <- st_intersection(circle, convex_hull)
+#' 
 #'     # Check the area of the intersection compared to the area of the circle
 #'     circle_area <- as.numeric(st_area(circle)) #as.numeric to remove units
-#'     
+#' 
 #'     intersection_area <- as.numeric(st_area(intersection)) #as.numeric to remove units
-#'     
+#' 
 #'     # Stop if the majority of the circle is outside the convex_hull
 #'     if (intersection_area / circle_area < 0.5) {
 #'       break
 #'     }
-#'     
+#' 
 #'     # Find points_sf that intersect the current circle
-#'     points_in_circle <- points_sf[st_intersects(points_sf, 
-#'                                                 circle, 
+#'     points_in_circle <- points_sf[st_intersects(points_sf,
+#'                                                 circle,
 #'                                                 sparse = FALSE), ]
-#'     
+#' 
 #'     # Add the points to the list
 #'     points_within_circles[[paste0("radius_", radius)]] <-
 #'       list(points=points_in_circle,circle=circle)
 #'   }
-#'   
+#' 
 #'   # Return the list of points within circles
 #'   return(points_within_circles)
 #' }
@@ -66,47 +66,47 @@
 #' {
 #'   # Initialize an empty data frame for the results
 #'   results_df <- data.frame(matrix(ncol = length(habitat_names)+
-#'                                     length(species_group_names)+2, 
+#'                                     length(species_group_names)+2,
 #'                                   nrow = 0))
 #'   colnames(results_df) <- c(habitat_names,"Area_Total",
 #'                             species_group_names,"Sp_Total")
-#'   
+#' 
 #'   # Iterate over each area sample (i.e. group of sites)
 #'   for (i in seq_along(samples)) {
 #'     sample <- samples[[i]]
 #'     polygon <- polygons[[i]]
-#'     
+#' 
 #'     # Crop and mask the raster to the polygon extent
 #'     habitat_cropped <- crop(habitat_raster, vect(polygon))
 #'     habitat_masked <- mask(habitat_cropped, vect(polygon))
-#'     
+#' 
 #'     # Calculate the area of each habitat type
 #'     habitat_df <- freq(habitat_masked, bylayer=FALSE)
-#'     habitat_df <- rbind(habitat_df,freq(habitat_masked, 
+#'     habitat_df <- rbind(habitat_df,freq(habitat_masked,
 #'                                         bylayer=FALSE,value=NA))
-#'     
+#' 
 #'     # Ensure all possible values are included
 #'     all_values_df <- data.frame(value = habitat_values, count = 0)
-#'     
+#' 
 #'     # Merge with actual frequency data, replacing 0 where missing
 #'     habitat_df <- merge(all_values_df, habitat_df,
 #'                         by = "value", all.x = TRUE)
-#'     
+#' 
 #'     # Fill NA counts with 0
-#'     habitat_df$count <- ifelse(is.na(habitat_df$count.y), 
+#'     habitat_df$count <- ifelse(is.na(habitat_df$count.y),
 #'                                0, habitat_df$count.y)
-#'     
+#' 
 #'     # Drop unnecessary column
 #'     habitat_df <- habitat_df[, c("value", "count")]
-#'     
-#'     habitat_df$area <- habitat_df$count * 
+#' 
+#'     habitat_df$area <- habitat_df$count *
 #'       res(habitat_raster)[1] * res(habitat_raster)[2]
-#'     
+#' 
 #'     # Store the results
 #'     results_df[i, seq_along(habitat_names)] <- habitat_df$area # store area of each habitat
-#'     results_df[i, length(habitat_names)+1] <- 
+#'     results_df[i, length(habitat_names)+1] <-
 #'       sum(results_df[i, seq_along(habitat_names)]) # store total area
-#'     
+#' 
 #'     # Subset species occurrences for each group
 #'     total_species <- 0
 #'     for (k in seq_along(species_groups))
@@ -117,12 +117,12 @@
 #'       results_df[i, length(habitat_names)+1+k] <- num_species #store species number in results
 #'       total_species <- total_species + num_species
 #'     }
-#'     
+#' 
 #'     #store the total number of species by summing across species groups
-#'     results_df[i, length(habitat_names) + 
+#'     results_df[i, length(habitat_names) +
 #'                  length(species_groups)+2] <- total_species
 #'   }
-#'   
+#' 
 #'   return(results_df)
 #' }
 #' 
@@ -135,17 +135,17 @@
 #' 
 #' create_squares <- function(points_sf, width) {
 #'   # Ensure the input is a point sf object
-#'   if (!inherits(points_sf, "sf") || !inherits(st_geometry(points_sf), 
+#'   if (!inherits(points_sf, "sf") || !inherits(st_geometry(points_sf),
 #'                                               "sfc_POINT")) {
 #'     stop("Input must be an sf object with point geometries.")
 #'   }
 #'   # Calculate half-width (to shift the square corners)
 #'   half_width <- width / 2
-#'   
-#'   squares_sf <- st_as_sf(st_buffer(points_sf, 
-#'                                    dist = half_width, 
+#' 
+#'   squares_sf <- st_as_sf(st_buffer(points_sf,
+#'                                    dist = half_width,
 #'                                    endCapStyle = "SQUARE"))
-#'   
+#' 
 #'   return(squares_sf)
 #' }
 #' 
@@ -158,14 +158,14 @@
 #' #' @importFrom sf st_geometry st_coordinates
 #' #' @importFrom stats kmeans
 #' 
-#' filter_points_in_clusters <- function(points_sf, squares_sf, 
-#'                                       cluster_size_vector) 
+#' filter_points_in_clusters <- function(points_sf, squares_sf,
+#'                                       cluster_size_vector)
 #' {
 #'   npoints <- nrow(points_sf)
 #'   n_clusters_vector = npoints%/%cluster_size_vector
-#'   n_clusters_vector[n_clusters_vector==0] <- 1  # when whole landscape, npoints<cluster_size 
+#'   n_clusters_vector[n_clusters_vector==0] <- 1  # when whole landscape, npoints<cluster_size
 #'   points_within_clusters<- list()
-#'   for (n_clusters in n_clusters_vector) 
+#'   for (n_clusters in n_clusters_vector)
 #'   {
 #'     if(n_clusters==npoints)
 #'     {
@@ -176,26 +176,26 @@
 #'     {
 #'       # Extract coordinates for k-means
 #'       coords <- st_coordinates(points_sf)
-#'       
+#' 
 #'       # Perform k-means clustering
 #'       kmeans_result <- kmeans(coords, centers = n_clusters)
-#'       
+#' 
 #'       # split the points sf into a list of clusters of points
 #'       points_in_clusters <- split(points_sf, kmeans_result$cluster)
 #'       squares_in_clusters <- split(squares_sf, kmeans_result$cluster)
-#'       
+#' 
 #'       # Merge all squares into a single geometry
 #'       merged_geom_within_clusters <- lapply(squares_in_clusters, st_union)
-#'       
+#' 
 #'       # Compute convex hull
-#'       clusters_convex_hulls <- lapply(merged_geom_within_clusters, 
+#'       clusters_convex_hulls <- lapply(merged_geom_within_clusters,
 #'                                       st_convex_hull)
-#'       
-#'       #clusters_convex_hulls <- lapply(points_in_clusters, 
+#' 
+#'       #clusters_convex_hulls <- lapply(points_in_clusters,
 #'       #                                   function(points) {st_convex_hull(st_union(points))})
 #'     }
 #'     # Add the points to the list
-#'     points_within_clusters[[paste0("size_", 
+#'     points_within_clusters[[paste0("size_",
 #'             cluster_size_vector[n_clusters_vector==n_clusters])]] <-
 #'       list(points=points_in_clusters,chulls=clusters_convex_hulls)
 #'   }
@@ -204,29 +204,122 @@
 #' }
 #' 
 #' 
-#' 
+#' #' @importFrom stats kmeans
 #' ##sumarize the samples for SAR type III
-#' extract_species_positions <- function(species_habitat_matrix, 
+#' extract_species_positions <- function(species_habitat_matrix,
 #'                                       species_site_matrix) {
 #'   # Get habitat names
 #'   habitat_names <- colnames(species_habitat_matrix[,-1])
-#'   
+#' 
 #'   # Initialize a list to store species positions for each habitat
-#'   habitat_positions <- list()
-#'   
+#'   habitat_positions <- vector("list", length = length(habitat_names))
+#' 
 #'   # Loop through each habitat
 #'   for (habitat in habitat_names) {
 #'     # Get species associated with this habitat
-#'     species_in_habitat <- 
+#'     species_in_habitat <-
 #'       rownames(species_habitat_matrix)[species_habitat_matrix[, habitat] == 1]
-#'     
+#' 
 #'     # Find positions (indices) of these species in the species-site matrix
-#'     species_positions <- which(rownames(species_site_matrix) %in% 
+#'     species_positions <- which(rownames(species_site_matrix) %in%
 #'                                  species_in_habitat)
-#'     
+#' 
 #'     # Store results in the list
 #'     habitat_positions[[habitat]] <- species_positions
 #'   }
-#'   
+#' 
 #'   return(habitat_positions)
+#' }
+#' 
+#' 
+#' #####################################################
+#' ######MAIN FUNCTION FOR BUILDING THE SAR####################
+#' ################################################
+#' 
+#' #' @description Build a SAR .....
+#' #' @usage sar_build(data, sp, lurast, crs = 4326,
+#' #' habNam, habVals, sq_width = 2000, cluster_size_vector)
+#' #' @param data A dataset in the form of a dataframe with two columns: the first
+#' #'   with island/site areas, and the second with the species richness of each
+#' #'   island/site.
+#' #' @param sp A vector of model names.
+#' #' @param lurast The test used to test the normality of the residuals of each
+#' #'   model. Can be any of "lillie" (Lilliefors Kolmogorov-Smirnov test),
+#' #'   "shapiro" (Shapiro-Wilk test of normality), "kolmo" (Kolmogorov-Smirnov
+#' #'   test), or "none" (no residuals normality test is undertaken; the default).
+#' #' @param habNam The test used to check for homogeneity of the residuals of
+#' #'   each model. Can be any of "cor.fitted" (a correlation of the squared
+#' #'   residuals with the model fitted values), "cor.area" (a correlation of the
+#' #'   squared residuals with the area values), or "none" (no residuals
+#' #'   homogeneity test is undertaken; the default).
+#' #' @param habVals The correlation test to be used when \code{homoTest !=
+#' #'   "none"}. Can be any of "spearman" (the default), "pearson", or "kendall".
+#' #' @param sq_width Should a grid search procedure be implemented to test
+#' #'   multiple starting parameter values. Can be one of 'none', 'partial' or
+#' #'   'exhaustive' The default is set to 'partial'.
+#' #' @param cluster_size_vector If \code{grid_start = exhaustive}, the number of points sampled
+#' #'   in the starting parameter space (see details).
+#' #' @details ***
+#' #' @return ***
+#' #' @importFrom sf st_convex_hull st_as_sf
+#' #' @examples
+#' #' data(galap)
+#' #'
+#' #' @export
+#' 
+#' 
+#' sar_build <- function(data, sp, lurast,
+#'                       crs = 4326,
+#'                       habNam,
+#'                       habVals,
+#'                       sq_width = 2000,
+#'                       cluster_size_vector){
+#' 
+#'   if (!is.data.frame(data) & !is.matrix(data)){
+#'     stop("data should be a dataframe or matrix")
+#'   }
+#'   data <- as.data.frame(data)
+#'   
+#'   if ((ncol(data) - 3) != nrow(sp)){
+#'     stop("number of species in data and sp does not match")
+#'   }
+#'   
+#'   colnames(sp)[1] <- "species"
+#'   if (!all(colnames(data[,-c(1:3)]) %in% sp$species)){
+#'     stop("species names in data and sp do not match")
+#'   }
+#'   
+#'   if (any(rowSums(sp[,-1]) != 1)){
+#'     stop("Row sums in sp are not all equal to 1")
+#'   }
+#'   
+#'   #Convert abundance to presence-absence
+#'   if (any(data[,-c(1:3)] > 1)){
+#'     w1 <- which(data[,-c(1:3)] > 1)
+#'     data[,-c(1:3)][w1] <- 1
+#'     warning("Some values in data not 1 - all such values converted to 1")
+#'   }
+#'   
+#'   
+#'   
+#'   points <- st_as_sf(data, coords=c("long","lat"), crs=crs)
+#' 
+#'   convex_hull <- st_convex_hull(st_union(points))
+#' 
+#'   squares_sf <- create_squares(points, width = sq_width)
+#' 
+#'   pt_in_clusters <- filter_points_in_clusters(points,
+#'                                               squares_sf,
+#'                                               cluster_size_vector)
+#' 
+#'   species_groups <- extract_species_positions(classif, points[,-1])
+#' 
+#'   samples <- lapply(pt_in_clusters, function(x) x$points)
+#'   polygons <- lapply(pt_in_clusters, function(x) x$chulls)
+#' 
+#'   res <- summarize_samples(list_flatten(samples), list_flatten(polygons),
+#'                            lurast, habNam, habVals, species_groups,
+#'                            names(species_groups))
+#' 
+#'   return(res)
 #' }
